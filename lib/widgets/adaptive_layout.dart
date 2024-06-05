5b236e3767860e79eb7d30b02cdd2cbff8cced42
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-06-05 09:04:47 -0700

    create adaptive layout widget and desktop layout widget

---
+++ lib/widgets/adaptive_layout.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.destopLayout});
+  final WidgetBuilder mobileLayout, tabletLayout, destopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constrains) {
+      if (constrains.maxWidth < 300) {
+        return mobileLayout(context);
+      } else if (constrains.maxWidth < 700) {
+        return tabletLayout(context);
+      } else {
+        return destopLayout(context);
+      }
+    });
+  }
+}
