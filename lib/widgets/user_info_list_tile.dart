0b92ebaaa6e1ef46c241662fc937ff7fb18aff05
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-06-05 10:25:28 -0700

    add user info part one

---
+++ lib/widgets/user_info_list_tile.dart
@@ -0,0 +1,30 @@
+import 'package:dashboard/utils/app_images.dart';
+import 'package:dashboard/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile({
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
+  final String title;
+  final String subtitle;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(image),
+      title: Text(
+        "{$title}",
+        style: AppStyles.styleSemiBold16,
+      ),
+      subtitle: Text(
+        "{$subtitle}",
+        style: AppStyles.styleRegular12,
+      ),
+    );
+  }
+}
