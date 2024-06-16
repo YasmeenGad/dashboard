9659c39fc9933f74eba99b594369f0d277e6fc7d
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-06-16 22:37:56 +0200

    create custom_backgrounf container

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,5 +1,6 @@
 import 'package:dashboard/widgets/all_expenses_header.dart';
 import 'package:dashboard/widgets/all_expenses_items_listview.dart';
+import 'package:dashboard/widgets/custom_background_container.dart';
 import 'package:flutter/material.dart';
 
 class AllExpenses extends StatelessWidget {
@@ -7,21 +8,20 @@ class AllExpenses extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
+    return CustomBackgroundContainer(
+       child: Column(
         children: [
           AllExpensesHeader(),
           const SizedBox(
             height: 16,
           ),
           const AllExpensesItemsListview(),
+
         ],
       ),
+      
     );
   }
 }
+
+
---
+++ lib/widgets/custom_background_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomBackgroundContainer extends StatelessWidget {
+  const CustomBackgroundContainer({
+    super.key,
+    this.child,
+    this.padding,
+  });
+  final child;
+  final double? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(padding ?? 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: child);
+  }
+}
