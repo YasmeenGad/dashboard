e6a040aa4432a12a6534b8cc13a98f12d5dd71bc
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-06-18 02:58:04 +0300

    doing document format

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,10 +2,10 @@ import 'package:dashboard/utils/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({this.backgroundColor, required this.text, this.textColor});
+  const CustomButton(
+      {this.backgroundColor, required this.text, this.textColor});
   final Color? backgroundColor, textColor;
   final String text;
-
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +16,10 @@ class CustomButton extends StatelessWidget {
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           elevation: 0,
-          backgroundColor: backgroundColor?? Color(0xFF4EB7F2),
+          backgroundColor: backgroundColor ?? Color(0xFF4EB7F2),
         ),
         onPressed: () {},
         child: FittedBox(
-          
           child: Text(
             text,
             style: AppStyles.styleSemiBold18.copyWith(color: textColor),
