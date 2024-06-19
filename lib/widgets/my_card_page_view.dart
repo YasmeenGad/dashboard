7f6817dd1b0109c7523d9b31267fc6edf6e55fec
Yasmeen Gad <gadyasmeen335@gmail.com>
2024-06-19 07:26:59 +0300

    add expandable page view to my card

--- lib/widgets/my_card_page_view.dart
+++ lib/widgets/my_card_page_view.dart
@@ -1,22 +1,18 @@
 import 'package:dashboard/widgets/my_card.dart';
+import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
 
 class MyCardPageView extends StatelessWidget {
   const MyCardPageView({super.key});
-  static const List<double> childrenHeight = [];
-  final int currentPage = 0;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: childrenHeight[currentPage],
-      child: PageView.builder(
-        
-        scrollDirection: Axis.horizontal,
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return MyCard();
-        },
-      ),
-    );
+    return ExpandablePageView(
+      scrollDirection: Axis.horizontal,
+        children: List.generate(
+      3,
+      (index) {
+        return MyCard();
+      },
+    ));
   }
 }
